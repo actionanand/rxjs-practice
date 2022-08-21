7587a3d940c815de87e04c808da119e95a64441d
Anand raja <meetanandns@gmail.com>
2022-08-21 19:20:03 +0000

    merge rxjs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
-import { concat, of } from 'rxjs';
+import { concat, interval, merge, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -13,13 +14,22 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // concat observable; output: 1, 2, 3, 4, 5, 6, 7, 8, 9
     const source1$ = of(1, 2, 3);
     const source2$ = of(4, 5, 6);
     const source3$ = of(7, 8, 9);
 
-    const result$ = concat(source1$, source2$, source3$);
+    const concatResult$ = concat(source1$, source2$, source3$);
 
-    result$.subscribe(val => console.log(val));
+    concatResult$.subscribe(val => console.log(val));
+
+
+    // merge observable; output: 0, 0, 1, 10, 2, 20, 3, 30 ...
+    const interval1$ = interval(1000);
+    const interval2$ = interval1$.pipe(map(val => val * 10));
+    const intervalResult$ = merge(interval1$, interval2$);
+
+    intervalResult$.subscribe(console.log);
   }
 
 }
