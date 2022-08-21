eb8231255db413ad71a7648a263b01af37394766
Anand raja <meetanandns@gmail.com>
2022-08-21 14:31:01 +0000

    concat rxjs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
-import { noop } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { concat, of } from 'rxjs';
 
-import { createHttpObservable } from '../common/util';
 
 @Component({
   selector: 'about',
@@ -15,7 +13,13 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const source1$ = of(1, 2, 3);
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
 
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$.subscribe(val => console.log(val));
   }
 
 }
